3606a6cb201b44ca7697efe585213c1013261219
Sanela <saneladovlete@yahoo.com>
2023-05-03 17:59:28 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
-import "./style.css";
+import "./index.css";
 
 export default function App() {
-  return "hi";
+  return (
+    <>
+      <form className="new-item-form">
+        <div clasName="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Shopping list</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
